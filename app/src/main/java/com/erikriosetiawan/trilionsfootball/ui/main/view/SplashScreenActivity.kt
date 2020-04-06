c836f066fc0cdc49a0eed1551421787622df9419
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-06 12:56:07 +0700

    Implement view model in the activity

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/SplashScreenActivity.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/SplashScreenActivity.kt
@@ -2,12 +2,24 @@ package com.erikriosetiawan.trilionsfootball.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.erikriosetiawan.trilionsfootball.R
+import com.erikriosetiawan.trilionsfootball.ui.main.viewmodel.SplashScreenViewModel
+import com.erikriosetiawan.trilionsfootball.ui.main.viewmodel.SplashScreenViewModelFactory
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        setUpViewModel()
+    }
+
+    /**
+     * Setup view model and view model factory for this activity
+     */
+    private fun setUpViewModel() {
+        val viewModelFactory = SplashScreenViewModelFactory(this)
+        ViewModelProviders.of(this, viewModelFactory).get(SplashScreenViewModel::class.java)
     }
 }
