7ea4c7415308b11fd429220cdd5b0ff0ac5ca27e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-06 11:16:08 +0700

    Add SplashScreenActivity.kt and activity_splash_screen.xml files

---
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.trilionsfootball.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.trilionsfootball.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
