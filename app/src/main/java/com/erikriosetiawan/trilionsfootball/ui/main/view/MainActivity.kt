4d19426833540b74490fbc9546f66ce10acee271
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-09 12:24:30 +0700

    Add dummy data

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.erikriosetiawan.trilionsfootball.ui.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.erikriosetiawan.trilionsfootball.R
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.trilionsfootball.adapter.LeagueAdapter
+import com.erikriosetiawan.trilionsfootball.data.model.League
+import com.erikriosetiawan.trilionsfootball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val leagues: MutableList<League> = mutableListOf()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setUI()
+    }
+
+    private fun setUI() {
+        setDummyData()
+        val adapter = LeagueAdapter(this, leagues)
+        binding.rvLeague.adapter = adapter
+    }
+
+    private fun setDummyData() {
+        for (i in 0..9) {
+            val league =
+                League(
+                    strBadge = "https://www.thesportsdb.com/images/media/league/badge/i6o0kh1549879062.png",
+                    strLeague = "League name $i"
+                )
+            leagues.add(league)
+        }
     }
 }
