1d2cf6e116058b9d42ead181586fab670a3958b6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-09 11:29:57 +0700

    Add model class

---
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/data/model/League.kt
@@ -0,0 +1,17 @@
+package com.erikriosetiawan.trilionsfootball.data.model
+
+data class League(
+    val idLeague: String? = null,
+    val strSport: String? = null,
+    val strLeague: String? = null,
+    val strLeagueAlternate: String? = null,
+    val strDivision: String? = null,
+    val strCurrentSeason: String? = null,
+    val intFormedYear: String? = null,
+    val strGender: String? = null,
+    val strCountry: String? = null,
+    val strWebsite: String? = null,
+    val strDescriptionEN: String? = null,
+    val strBadge: String? = null,
+    val strLogo: String? = null
+)
