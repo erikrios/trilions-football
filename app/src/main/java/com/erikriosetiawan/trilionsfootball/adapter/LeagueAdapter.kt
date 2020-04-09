857224faf1bdd74ffa0d65dcc1d08215565bf6ae
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-09 19:10:12 +0700

    Set different view background

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/adapter/LeagueAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/adapter/LeagueAdapter.kt
@@ -3,7 +3,9 @@ package com.erikriosetiawan.trilionsfootball.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.trilionsfootball.R
 import com.erikriosetiawan.trilionsfootball.data.model.League
 import com.erikriosetiawan.trilionsfootball.databinding.ItemLeagueBinding
 import com.squareup.picasso.Picasso
@@ -20,17 +22,19 @@ class LeagueAdapter(private val context: Context, private val leagues: List<Leag
     override fun getItemCount(): Int = leagues.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        holder.bind(leagues[position]) {
+        holder.bind(leagues[position], position) {
             // Intent code here
         }
-
 
     inner class ViewHolder(private val binding: ItemLeagueBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(league: League, clickListener: (League) -> Unit) {
+        fun bind(league: League, position: Int, clickListener: (League) -> Unit) {
             Picasso.get().load(league.strBadge).into(binding.imgLeagueLogo)
             binding.tvLeagueName.text = league.strLeague
+            if (position % 2 == 1)
+                binding.viewBackground.background =
+                    ContextCompat.getDrawable(context, R.drawable.bg_league_card_two)
             itemView.setOnClickListener { clickListener(league) }
         }
     }
