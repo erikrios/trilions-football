582ef84c3b6fe817b0448d11de04c8efbce45e9a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-09 12:00:06 +0700

    Add LeagueAdapter.kt

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/adapter/LeagueAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/adapter/LeagueAdapter.kt
@@ -1,28 +1,37 @@
 package com.erikriosetiawan.trilionsfootball.adapter
 
 import android.content.Context
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.erikriosetiawan.trilionsfootball.data.model.League
+import com.erikriosetiawan.trilionsfootball.databinding.ItemLeagueBinding
+import com.squareup.picasso.Picasso
 
-class LeagueAdapter(context: Context, leagues: List<League>) :
+class LeagueAdapter(private val context: Context, private val leagues: List<League>) :
     RecyclerView.Adapter<LeagueAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(context)
+        val binding = ItemLeagueBinding.inflate(layoutInflater)
+        return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = leagues.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(leagues[position]) {
+            // Intent code here
+        }
 
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val binding: ItemLeagueBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(league: League, clickListener: (League) -> Unit) {
+            Picasso.get().load(league.strBadge).into(binding.imgLeagueLogo)
+            binding.tvLeagueName.text = league.strLeague
+            itemView.setOnClickListener { clickListener(league) }
+        }
     }
 }
