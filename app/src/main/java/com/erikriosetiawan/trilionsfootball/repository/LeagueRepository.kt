f1992d850e577683aad595fc84341ce09dfa7bce
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 20:13:29 +0700

    Create LeagueRepository.kt

---
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/repository/LeagueRepository.kt
@@ -0,0 +1,16 @@
+package com.erikriosetiawan.trilionsfootball.repository
+
+import com.erikriosetiawan.trilionsfootball.data.model.League
+import com.erikriosetiawan.trilionsfootball.datastore.league.LeagueDataStore
+
+class LeagueRepository private constructor() : BaseRepository<LeagueDataStore>() {
+
+    suspend fun getLeagues(): MutableList<League>? {
+        val cache = localDataStore?.getLeagues()
+        if (cache != null) return cache as MutableList<League>
+        val response = remoteDataStore?.getLeagues()
+        remoteDataStore?.getLeagues()
+        localDataStore?.addAll(response)
+        return response as MutableList<League>
+    }
+}
