e58d7acdc5b72aaa7db81220a67c67b1c1d1768c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 19:56:56 +0700

    Change the return value

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueDataStore.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueDataStore.kt
@@ -1,9 +1,10 @@
 package com.erikriosetiawan.trilionsfootball.datastore.league
 
+import com.erikriosetiawan.trilionsfootball.data.model.League
 import com.erikriosetiawan.trilionsfootball.data.model.LeagueResponse
 
 interface LeagueDataStore {
 
-    suspend fun getLeagues(): LeagueResponse?
-    suspend fun addAll(leagues: LeagueResponse?)
+    suspend fun getLeagues(): List<League>?
+    suspend fun addAll(leagues: List<League>?)
 }
