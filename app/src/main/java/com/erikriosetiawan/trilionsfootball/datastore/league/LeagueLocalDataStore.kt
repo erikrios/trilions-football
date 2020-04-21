913f8292bbb6bee74b48dbfd4153962d76e8a60e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 19:58:22 +0700

    Change the return value

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueLocalDataStore.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueLocalDataStore.kt
@@ -1,16 +1,17 @@
 package com.erikriosetiawan.trilionsfootball.datastore.league
 
+import com.erikriosetiawan.trilionsfootball.data.model.League
 import com.erikriosetiawan.trilionsfootball.data.model.LeagueResponse
 
 class LeagueLocalDataStore : LeagueDataStore {
 
-    private var caches = LeagueResponse()
+    private var caches = mutableListOf<League>()
 
-    override suspend fun getLeagues(): LeagueResponse? =
-        if (caches.leagues.isNotEmpty()) caches
+    override suspend fun getLeagues(): List<League>? =
+        if (caches.isEmpty()) caches
         else null
 
-    override suspend fun addAll(leagues: LeagueResponse?) {
-        leagues?.let { caches = it }
+    override suspend fun addAll(leagues: List<League>?) {
+        leagues?.let { caches = it as MutableList<League> }
     }
 }
