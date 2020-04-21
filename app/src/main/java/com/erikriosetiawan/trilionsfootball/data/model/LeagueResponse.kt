95db390a43b3ad7e4936d63b85788d46edb2e675
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 19:19:05 +0700

    Add SerializedName

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/data/model/LeagueResponse.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/data/model/LeagueResponse.kt
@@ -1,5 +1,8 @@
 package com.erikriosetiawan.trilionsfootball.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LeagueResponse(
+    @SerializedName("countrys")
     val leagues: List<League>? = null
 )
