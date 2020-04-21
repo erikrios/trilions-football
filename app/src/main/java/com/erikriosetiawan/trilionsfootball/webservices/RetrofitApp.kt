8f8722282498b4bc3e8b2cc099b46673eb07a17f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 21:26:42 +0700

    Add logging to track the request and response from API

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/webservices/RetrofitApp.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/webservices/RetrofitApp.kt
@@ -1,15 +1,25 @@
 package com.erikriosetiawan.trilionsfootball.webservices
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitApp {
 
     private const val BASE_URL = "https://www.thesportsdb.com/api/v1/json/1/"
 
+    private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttp = OkHttpClient.Builder()
+        .callTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(logger)
+
     private val client = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp.build())
         .build()
 
     val THE_SPORT_DB_SERVICE: TheSportDbService = client.create(TheSportDbService::class.java)
