ebe97ff3aa57d6af056d2b86c57a3f7dea7626a4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-21 20:00:15 +0700

    Create LeagueRemoteDataStore.kt

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueRemoteDataStore.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/datastore/league/LeagueRemoteDataStore.kt
@@ -1,1 +1,20 @@
 package com.erikriosetiawan.trilionsfootball.datastore.league
+
+import com.erikriosetiawan.trilionsfootball.data.model.League
+import com.erikriosetiawan.trilionsfootball.data.model.LeagueResponse
+import com.erikriosetiawan.trilionsfootball.webservices.TheSportDbService
+
+class LeagueRemoteDataStore(private val theSportDbService: TheSportDbService) : LeagueDataStore {
+
+    override suspend fun getLeagues(): List<League>? {
+        val response = theSportDbService.getLeagues("England", "Soccer")
+        if (response.isSuccessful)
+            return response.body()?.leagues
+
+        throw Exception("A problem happened when request data, status error ${response.code()}")
+    }
+
+    override suspend fun addAll(leagues: List<League>?) {
+
+    }
+}
