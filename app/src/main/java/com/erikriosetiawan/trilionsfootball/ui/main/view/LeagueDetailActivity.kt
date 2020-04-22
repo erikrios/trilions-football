04b86f39583e79a3f3756b6f47591779ae49c3e6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-22 15:47:56 +0700

    Create a method to create show the data

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.trilionsfootball.data.model.League
 import com.erikriosetiawan.trilionsfootball.databinding.ActivityLeagueDetailBinding
+import com.squareup.picasso.Picasso
 
 class LeagueDetailActivity : AppCompatActivity() {
 
@@ -17,6 +18,17 @@ class LeagueDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLeagueDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        showData()
+    }
+
+    private fun showData() {
+        val league = getDataIntent()
+        league?.apply {
+            Picasso.get().load(strLogo).into(binding.imgLeagueLogo)
+            binding.tvLeagueName.text = strLeague
+            binding.tvLeagueDescription.text = strDescriptionEN
+        }
     }
 
     private fun getDataIntent(): League? = intent.getParcelableExtra(DATA_LEAGUE_KEY)
