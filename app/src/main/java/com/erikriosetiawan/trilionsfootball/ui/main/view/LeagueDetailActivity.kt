31330bb4fbc29558d3b2bafb1aced948665359be
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-22 15:33:45 +0700

    Integrate view binding with LeagueDetailActivity.kt

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
@@ -3,11 +3,15 @@ package com.erikriosetiawan.trilionsfootball.ui.main.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.erikriosetiawan.trilionsfootball.R
+import com.erikriosetiawan.trilionsfootball.databinding.ActivityLeagueDetailBinding
 
 class LeagueDetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLeagueDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league_detail)
+        binding = ActivityLeagueDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
