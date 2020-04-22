2a04f762efc33200dfcf63aba083d3637ddeb917
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-22 15:36:57 +0700

    Create key for intent

--- app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
+++ app/src/main/java/com/erikriosetiawan/trilionsfootball/ui/main/view/LeagueDetailActivity.kt
@@ -8,6 +8,10 @@ class LeagueDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLeagueDetailBinding
 
+    companion object {
+        const val DATA_LEAGUE_KEY = "data_league_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLeagueDetailBinding.inflate(layoutInflater)
